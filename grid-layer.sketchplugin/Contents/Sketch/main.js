ba71f4891da7607236995763f2e308aaa717d535
Jonathan Tran <Jon@jon-tm00316-imac.lon.thomac.net>
2019-06-17 14:37:39 +0100

    update manifest

--- grid-layer.sketchplugin/Contents/Sketch/main.js
+++ grid-layer.sketchplugin/Contents/Sketch/main.js
@@ -1,25 +1,458 @@
-var that=this;function __skpm_run(t,e){that.context=e;var r=function(t){var e={};function r(n){if(e[n])return e[n].exports;var u=e[n]={i:n,l:!1,exports:{}};return t[n].call(u.exports,u,u.exports,r),u.l=!0,u.exports}return r.m=t,r.c=e,r.d=function(t,e,n){r.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:n})},r.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},r.t=function(t,e){if(1&e&&(t=r(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var u in t)r.d(n,u,function(e){return t[e]}.bind(null,u));return n},r.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return r.d(e,"a",e),e},r.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},r.p="",r(r.s="./src/main.js")}({"./src/inputModal.js":
+var that = this;
+function __skpm_run (key, context) {
+  that.context = context;
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/inputModal.js":
 /*!***************************!*\
   !*** ./src/inputModal.js ***!
   \***************************/
-/*! exports provided: default */function(t,e,r){"use strict";r.r(e),e.default=function(t,e,n,u){var i=r(/*! sketch/ui */"sketch/ui"),o=r(/*! sketch/settings */"sketch/settings"),a=!0,s=null,f=!0,c="Column amount",m=null,l=!0,h=!0,d="Gutter width",p=null,g=!0,y=o.settingForKey(n),b=o.settingForKey(u);if(m=y||12,p=b||12,i.getInputFromUser(t+" - Gutters on the outside?",{type:i.INPUT_TYPE.selection,possibleValues:["Yes","No"]},function(t,e){t?a=!1:s=e}),a){for(;f;)i.getInputFromUser(c,{initialValue:m},function(t,e){if(t)return l=!1,g=!1,void(f=!1);o.setSettingForKey(n,e),m=e,c="Column amount - is not a number"}),isNaN(m)||(f=!1),m<=0&&(c="Column amount - enter a number above 0",f=!0);if(l)for(;h;)if(i.getInputFromUser(d,{initialValue:p},function(t,e){if(t)return g=!1,void(h=!1);o.setSettingForKey(u,e),p=e,d="Gutter width - is not a number"}),isNaN(p)||(h=!1),p<=e){d=0==e?"Gutter width - enter a number above 0":"Gutter width - enter a positive number",h=!0}}return{outsideGutter:s,colAmount:m,gutterWidth:p,inputComplete:g}}},"./src/main.js":
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (function (type, min, key1, key2) {
+  var UI = __webpack_require__(/*! sketch/ui */ "sketch/ui");
+
+  var Settings = __webpack_require__(/*! sketch/settings */ "sketch/settings");
+
+  var selectorComplete = true;
+  var options = ["Yes", "No"];
+  var outsideGutter = null;
+  var colAmountIsNan = true;
+  var colAmountMessage = "Column amount";
+  var colAmount = null;
+  var colAmountComplete = true;
+  var gutterWidthIsNaN = true;
+  var gutterWidthMessage = "Gutter width";
+  var gutterWidth = null;
+  var inputComplete = true;
+  var settingsCol = Settings.settingForKey(key1);
+  var settingsGutter = Settings.settingForKey(key2);
+
+  if (settingsCol) {
+    colAmount = settingsCol;
+  } else {
+    colAmount = 12;
+  }
+
+  if (settingsGutter) {
+    gutterWidth = settingsGutter;
+  } else {
+    gutterWidth = 12;
+  }
+
+  UI.getInputFromUser(type + " - Gutters on the outside?", {
+    type: UI.INPUT_TYPE.selection,
+    possibleValues: options
+  }, function (err, value) {
+    if (err) {
+      selectorComplete = false;
+      return;
+    }
+
+    outsideGutter = value;
+  });
+
+  if (selectorComplete) {
+    while (colAmountIsNan) {
+      UI.getInputFromUser(colAmountMessage, {
+        initialValue: colAmount
+      }, function (err, value) {
+        if (err) {
+          colAmountComplete = false;
+          inputComplete = false;
+          colAmountIsNan = false;
+          return;
+        }
+
+        Settings.setSettingForKey(key1, value);
+        colAmount = value;
+        colAmountMessage = "Column amount - is not a number";
+      });
+
+      if (!isNaN(colAmount)) {
+        colAmountIsNan = false;
+      }
+
+      if (colAmount <= 0) {
+        colAmountMessage = "Column amount - enter a number above 0";
+        colAmountIsNan = true;
+      }
+    }
+
+    if (colAmountComplete) {
+      while (gutterWidthIsNaN) {
+        UI.getInputFromUser(gutterWidthMessage, {
+          initialValue: gutterWidth
+        }, function (err, value) {
+          if (err) {
+            inputComplete = false;
+            gutterWidthIsNaN = false;
+            return;
+          }
+
+          Settings.setSettingForKey(key2, value);
+          gutterWidth = value;
+          gutterWidthMessage = "Gutter width - is not a number";
+        });
+
+        if (!isNaN(gutterWidth)) {
+          gutterWidthIsNaN = false;
+        }
+
+        if (gutterWidth <= min) {
+          var message = null;
+
+          if (min == 0) {
+            message = "Gutter width - enter a number above 0";
+          } else {
+            message = "Gutter width - enter a positive number";
+          }
+
+          gutterWidthMessage = message;
+          gutterWidthIsNaN = true;
+        }
+      }
+    }
+  }
+
+  return {
+    outsideGutter: outsideGutter,
+    colAmount: colAmount,
+    gutterWidth: gutterWidth,
+    inputComplete: inputComplete
+  };
+});
+
+/***/ }),
+
+/***/ "./src/main.js":
 /*!*********************!*\
   !*** ./src/main.js ***!
   \*********************/
-/*! exports provided: gutters, columns */function(t,e,r){"use strict";r.r(e),r.d(e,"gutters",function(){return l}),r.d(e,"columns",function(){return h});var n=r(/*! sketch */"sketch"),u=r.n(n),i=r(/*! ./inputModal */"./src/inputModal.js"),o=u.a.getSelectedDocument().selectedLayers,a=o.length,s=r(/*! sketch/dom */"sketch/dom").Shape,f=r(/*! sketch/dom */"sketch/dom").Group,c=r(/*! sketch/ui */"sketch/ui"),m="Select a layer";function l(){if(0===a)return c.message(m);var t=Object(i.default)("GUTTERS",0,"gutterKey1","gutterKey2"),e=function(e,r){o.forEach(function(n){var u=parseInt(t.colAmount)+e,i=(n.frame.width-u*t.gutterWidth)/t.colAmount,o=function(e,r){return Math.trunc(n.frame.x+t.gutterWidth*e+i*(e+r))},a=t.gutterWidth*u;if(!(n.frame.width>a))return c.message("Gutters are too big for "+n.name);for(var m=new f({name:"Gutter",parent:n.parent}),l=0;l<u;l++)new s({name:"Gutter",parent:m,frame:{height:n.frame.height,width:t.gutterWidth,x:o(l,r),y:n.frame.y},style:{fills:["#ff0000"],opacity:.5,borders:[{thickness:0}]}});m.adjustToFit()})};"Yes"===t.outsideGutter&&t.inputComplete&&e(1,0),"No"===t.outsideGutter&&t.inputComplete&&e(-1,1)}function h(){if(0===a)return c.message(m);var t=Object(i.default)("COLUMNS",-1,"colKey1","colKey2");o.forEach(function(e){var r=[],n=function(n,u){for(var i=parseInt(t.colAmount)+n,o=t.gutterWidth*i,a=(e.frame.width-i*t.gutterWidth)/t.colAmount,s=function(r,n){return Math.trunc(e.frame.x+t.gutterWidth*r+a*(r+n))},f=0;f<i;f++)r.push(s(f,u));return o},u=function(n){var u=0,i=new f({name:"Columns",parent:e.parent});i.frame.x=e.frame.x,i.frame.y=e.frame.y,i.frame.width=e.frame.width,i.frame.height=e.frame.height;for(var o=0;o<r.length-1;o++){var a=r[o+1]-r[o]-Number(t.gutterWidth);u+=a;var c=t.gutterWidth*o+u-a+n,m=new s({name:"Column",parent:i,frame:{height:e.frame.height,x:c,width:a,y:0},style:{fills:["#00A2FF"],borders:[{thickness:0}]}});0==t.gutterWidth?m.style.opacity=o%2==0?.4:.5:m.style.opacity=.4}};if("Yes"===t.outsideGutter){if(!(e.frame.width>n(1,0)))return c.message("Gutters are too big for "+e.name);t.inputComplete&&u(Number(t.gutterWidth))}if("No"===t.outsideGutter){if(!(e.frame.width>n(-1,1)))return c.message("Gutters are too big for "+e.name);r.unshift(e.frame.x-t.gutterWidth),r.push(e.frame.x+e.frame.width),t.inputComplete&&u(0)}})}},sketch:
+/*! exports provided: gutters, columns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "gutters", function() { return gutters; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "columns", function() { return columns; });
+/* harmony import */ var sketch__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! sketch */ "sketch");
+/* harmony import */ var sketch__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(sketch__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _inputModal__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./inputModal */ "./src/inputModal.js");
+
+
+var document = sketch__WEBPACK_IMPORTED_MODULE_0___default.a.getSelectedDocument();
+var selectedLayers = document.selectedLayers;
+var selectedCount = selectedLayers.length;
+
+var Shape = __webpack_require__(/*! sketch/dom */ "sketch/dom").Shape;
+
+var Group = __webpack_require__(/*! sketch/dom */ "sketch/dom").Group;
+
+var UI = __webpack_require__(/*! sketch/ui */ "sketch/ui");
+
+var selectErrorMsg = "Select a layer";
+function gutters() {
+  if (selectedCount === 0) {
+    return UI.message(selectErrorMsg);
+  }
+
+  var input = Object(_inputModal__WEBPACK_IMPORTED_MODULE_1__["default"])("GUTTERS", 0, "gutterKey1", "gutterKey2");
+
+  var createGrid = function createGrid(num, val) {
+    selectedLayers.forEach(function (item) {
+      var gutterAmount = parseInt(input.colAmount) + num;
+      var colWidth = (item.frame.width - gutterAmount * input.gutterWidth) / input.colAmount;
+
+      var gutterX = function gutterX(index, val1) {
+        return Math.trunc(item.frame.x + input.gutterWidth * index + colWidth * (index + val1));
+      };
+
+      var totalWidth = input.gutterWidth * gutterAmount;
+
+      if (item.frame.width > totalWidth) {
+        var group = new Group({
+          name: "Gutter",
+          parent: item.parent
+        });
+
+        for (var i = 0; i < gutterAmount; i++) {
+          var gutter = new Shape({
+            name: "Gutter",
+            parent: group,
+            frame: {
+              height: item.frame.height,
+              width: input.gutterWidth,
+              x: gutterX(i, val),
+              y: item.frame.y
+            },
+            style: {
+              fills: ["#ff0000"],
+              opacity: 0.5,
+              borders: [{
+                thickness: 0
+              }]
+            }
+          });
+        }
+
+        group.adjustToFit();
+      } else {
+        return UI.message("Gutters are too big for " + item.name);
+      }
+    });
+  };
+
+  if (input.outsideGutter === "Yes") {
+    if (input.inputComplete) {
+      createGrid(1, 0);
+    }
+  }
+
+  if (input.outsideGutter === "No") {
+    if (input.inputComplete) {
+      createGrid(-1, 1);
+    }
+  }
+}
+function columns() {
+  if (selectedCount === 0) {
+    return UI.message(selectErrorMsg);
+  }
+
+  var input = Object(_inputModal__WEBPACK_IMPORTED_MODULE_1__["default"])("COLUMNS", -1, "colKey1", "colKey2");
+  selectedLayers.forEach(function (item) {
+    var gutterXList = [];
+
+    var calGutterX = function calGutterX(num, val) {
+      var gutterAmount = parseInt(input.colAmount) + num;
+      var totalWidth = input.gutterWidth * gutterAmount;
+      var colWidth = (item.frame.width - gutterAmount * input.gutterWidth) / input.colAmount;
+
+      var gutterX = function gutterX(index, val1) {
+        return Math.trunc(item.frame.x + input.gutterWidth * index + colWidth * (index + val1));
+      };
+
+      for (var i = 0; i < gutterAmount; i++) {
+        gutterXList.push(gutterX(i, val));
+      }
+
+      return totalWidth;
+    };
+
+    var createGrid = function createGrid(val) {
+      var currentWidth = 0;
+      var group = new Group({
+        name: "Columns",
+        parent: item.parent
+      });
+      group.frame.x = item.frame.x;
+      group.frame.y = item.frame.y;
+      group.frame.width = item.frame.width;
+      group.frame.height = item.frame.height;
+
+      for (var i = 0; i < gutterXList.length - 1; i++) {
+        var columnWidth = gutterXList[i + 1] - gutterXList[i] - Number(input.gutterWidth);
+        currentWidth = currentWidth + columnWidth;
+        var columnX = input.gutterWidth * i + currentWidth - columnWidth + val;
+        var column = new Shape({
+          name: "Column",
+          parent: group,
+          frame: {
+            height: item.frame.height,
+            x: columnX,
+            width: columnWidth,
+            y: 0
+          },
+          style: {
+            fills: ["#00A2FF"],
+            borders: [{
+              thickness: 0
+            }]
+          }
+        });
+
+        if (input.gutterWidth == 0) {
+          if (i % 2 == 0) {
+            column.style.opacity = 0.4;
+          } else column.style.opacity = 0.5;
+        } else {
+          column.style.opacity = 0.4;
+        }
+      }
+    };
+
+    if (input.outsideGutter === "Yes") {
+      if (item.frame.width > calGutterX(1, 0)) {
+        if (input.inputComplete) {
+          createGrid(Number(input.gutterWidth));
+        }
+      } else {
+        return UI.message("Gutters are too big for " + item.name);
+      }
+    }
+
+    if (input.outsideGutter === "No") {
+      if (item.frame.width > calGutterX(-1, 1)) {
+        gutterXList.unshift(item.frame.x - input.gutterWidth);
+        gutterXList.push(item.frame.x + item.frame.width);
+
+        if (input.inputComplete) {
+          createGrid(0);
+        }
+      } else {
+        return UI.message("Gutters are too big for " + item.name);
+      }
+    }
+  });
+}
+
+/***/ }),
+
+/***/ "sketch":
 /*!*************************!*\
   !*** external "sketch" ***!
   \*************************/
-/*! no static exports found */function(t,e){t.exports=require("sketch")},"sketch/dom":
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch");
+
+/***/ }),
+
+/***/ "sketch/dom":
 /*!*****************************!*\
   !*** external "sketch/dom" ***!
   \*****************************/
-/*! no static exports found */function(t,e){t.exports=require("sketch/dom")},"sketch/settings":
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch/dom");
+
+/***/ }),
+
+/***/ "sketch/settings":
 /*!**********************************!*\
   !*** external "sketch/settings" ***!
   \**********************************/
-/*! no static exports found */function(t,e){t.exports=require("sketch/settings")},"sketch/ui":
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch/settings");
+
+/***/ }),
+
+/***/ "sketch/ui":
 /*!****************************!*\
   !*** external "sketch/ui" ***!
   \****************************/
-/*! no static exports found */function(t,e){t.exports=require("sketch/ui")}});"default"===t&&"function"==typeof r?r(e):r[t](e)}that.gutters=__skpm_run.bind(this,"gutters"),that.onRun=__skpm_run.bind(this,"default"),that.columns=__skpm_run.bind(this,"columns");
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch/ui");
+
+/***/ })
+
+/******/ });
+  if (key === 'default' && typeof exports === 'function') {
+    exports(context);
+  } else {
+    exports[key](context);
+  }
+}
+that['gutters'] = __skpm_run.bind(this, 'gutters');
+that['onRun'] = __skpm_run.bind(this, 'default');
+that['columns'] = __skpm_run.bind(this, 'columns')
+
+//# sourceMappingURL=main.js.map
